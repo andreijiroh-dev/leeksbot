70b8674e72b3ce9c3e951816f250f37e465418a6
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-01-02 23:09:25 +0800

    feat(devenv.sh): :technologist: setup Nix devenv setup via https://devenv.sh
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages =  with pkgs; [
+                    gitFull
+                    prisma-engines
+                    openssl
+                  ];
+
+                  languages.javascript = {
+                    enable = true;
+                    package = pkgs.nodejs-slim_22;
+                    npm = {
+                      enable = true;
+                      install.enable = true;
+                    };
+                  };
+
+                  services.postgres = {
+                    enable = true;
+                    package = pkgs.postgresql_17;
+                  };
+
+                  dotenv.disableHint = true;
+
+                  env = {
+                    PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+                    PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+                    PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+                  };
+                }
+              ];
+            };
+          });
+    };
+}
